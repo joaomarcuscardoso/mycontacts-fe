b3d2b8aada55dc8fedfb1f2effbbd1e70b329e30
joaomarcuscardoso <rfcjoujou@gmail.com>
2021-12-23 02:33:38 -0300

    Card

--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -10,7 +10,6 @@ export const Header = styled.header`
   justify-content:space-between;
 
   strong {
-    color: #222222;
     font-size: 24px;
   }
 
@@ -30,4 +29,75 @@ export const Header = styled.header`
 
   }
 
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    margin-bottom: 8px;
+
+    button {
+      background: transparent;
+      border:none;
+      display:flex;
+      align-items: center;
+    }
+
+    span {
+      margin-right: 8px;
+      font-weight:bold;
+      color: ${({ theme }) => theme.colors.primary.main};
+
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0,0,0,0.4);
+  padding: 16px;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content:space-between;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  .info {
+    .contact-name {
+      display:flex;
+      align-items: center;
+
+      small {
+        background: ${({ theme }) => theme.colors.primary.lighter};
+        color: ${({ theme }) => theme.colors.primary.main};
+        font-weight: bold;
+        text-transform: uppercase;
+        padding: 4px;
+        border-radius: 4px;
+        margin-left:8px;
+      }
+    }
+
+    span {
+      display:block;
+      color: ${({ theme }) => theme.colors.gray[200]};
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+
+    button {
+      background: transparent;
+      border: none;
+      margin-left: 8px;
+
+    }
+  }
 `;
